393ca5ccf43439a41ac618372a8885c7ed17b5fb
Scott M Parrish <anithri@gmail.com>
2012-03-27 16:35:13 -0600

    updated model generator

--- lib/generators/grid_data/model/model_generator.rb
+++ lib/generators/grid_data/model/model_generator.rb
@@ -6,7 +6,8 @@ module GridData
     def create_model_file
       path = GridData.settings.config_dir
       last_model = Dir.glob("#{path}/*_*_model.*").last
-      next_num = File.basename(last_model).match(/^(\d+)_/)[1].succ
+      next_num = "100"
+      next_num = File.basename(last_model).match(/^(\d+)_/)[1].succ if last_model
       model_target = "#{path}/#{next_num}_#{model_name.underscore}_model.#{GridData.settings.yaml_extension}"
       warn model_target
       template 'model.yaml.tt', model_target
